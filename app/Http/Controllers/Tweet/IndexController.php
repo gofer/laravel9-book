771ee444a0774e48a2dea4fe9ddb409cce98c746
Gofer <gofer.ex.studio@gmail.com>
2023-10-30 00:01:43 +0900

    HTMLを表示する

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\View\Factory;
 
 class IndexController extends Controller
 {
@@ -11,10 +13,23 @@ class IndexController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\View\Factory  $factory
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Factory $factory)
     {
-        return 'Single Action!';
+        // 1. viewヘルパー関数で呼び出す
+        // return view('tweet.index', ['name' => 'laravel']);
+
+        // 2. Facadeで呼び出す
+        //return View::make('tweet.index', ['name' => 'laravel']);
+
+        // 3. Factoryをインジェクションして呼び出す
+        // return $factory->make('tweet.index', ['name' => 'laravel']);
+
+        // 4. viewヘルパー関数に変数を渡す別の方法
+        return view('tweet.index')
+            ->with('name', 'laravel')
+            ->with('version', '9');
     }
 }
